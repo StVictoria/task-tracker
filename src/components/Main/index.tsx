93e1e2b6f95b90169e5be635bf195866668aa82c
Viktoriia <v.stepnova@qlab.studio>
2022-12-22 12:11:48 +0300

    create sidebar

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,56 +1,18 @@
-import { Drawer, List, ListItem, ListItemButton } from '@mui/material'
 import { FC, useState } from 'react'
-import { IMenu, menu } from '../../static/menu'
 import Account from '../Account'
 import MyList from '../MyList'
+import Sidebar from '../Sidebar'
 import s from './styles.module.sass'
 
 const Main: FC = () => {
   const [currentSection, setCurrentSection] = useState('account')
 
-  // const navigate= useNavigate()
-  // const noAuth = true
-  // useEffect(() => {
-  //   if (noAuth) navigate('/auth')
-  // }, [])
-
-  const renderMenu = () =>
-    menu.map((item: IMenu) => (
-      <ListItem
-        key={item.id}
-        disablePadding
-        selected={item.to === currentSection}
-        className={s.sidebarListItem}
-        onClick={() => setCurrentSection(item.to)}
-      >
-        <ListItemButton>
-          <item.icon color='primary' />
-          <p className={s.sidebarLinkTitle}>{item.title}</p>
-        </ListItemButton>
-      </ListItem>
-    ))
-
   return (
     <>
-      <Drawer
-        anchor='left'
-        variant='persistent'
-        open={true}
-        className={s.sidebar}
-      >
-        <div>
-          <h1 className={s.logo}>Task Tracker</h1>
-          <div className={s.divider} />
-          <List>{renderMenu()}</List>
-        </div>
-        <footer className={s.authorInfo}>
-          Crafted by{' '}
-          <a href='https://github.com/StVictoria' className={s.authorLink}>
-            StVictoria
-          </a>
-          , 2022
-        </footer>
-      </Drawer>
+      <Sidebar
+        currentSection={currentSection}
+        setCurrentSection={setCurrentSection}
+      />
       <main className={s.main}>
         {currentSection === 'account' && <Account />}
         {currentSection === 'my-list' && <MyList />}
@@ -61,3 +23,4 @@ const Main: FC = () => {
 }
 
 export default Main
+
---
+++ src/components/Sidebar/index.tsx
@@ -0,0 +1,56 @@
+import { FC } from 'react'
+import { Drawer, List, ListItem, ListItemButton } from '@mui/material'
+import { IMenu, menu } from '../../static/menu'
+import s from './styles.module.sass'
+
+interface ISidebar {
+  currentSection: string
+  setCurrentSection: (currentSection: string) => void
+}
+
+const Sidebar: FC<ISidebar> = ({ currentSection, setCurrentSection }) => {
+  const renderMenu = () =>
+    menu.map((item: IMenu) => (
+      <ListItem
+        key={item.id}
+        disablePadding
+        selected={item.to === currentSection}
+        className={s.sidebarListItem}
+        onClick={() => setCurrentSection(item.to)}
+      >
+        <ListItemButton>
+          <item.icon color='primary' />
+          <p className={s.sidebarLinkTitle}>{item.title}</p>
+        </ListItemButton>
+      </ListItem>
+    ))
+
+  return (
+    <Drawer
+      anchor='left'
+      variant='persistent'
+      open={true}
+      className={s.sidebar}
+    >
+      <div>
+        <h1 className={s.logo}>Task Tracker</h1>
+        <div className={s.divider} />
+        <List>{renderMenu()}</List>
+      </div>
+      <footer className={s.authorInfo}>
+        Crafted by{' '}
+        <a
+          href='https://github.com/StVictoria'
+          target='_blank'
+          rel='noreferrer'
+          className={s.authorLink}
+        >
+          StVictoria
+        </a>
+        , 2022
+      </footer>
+    </Drawer>
+  )
+}
+
+export default Sidebar
